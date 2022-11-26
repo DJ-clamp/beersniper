ac77cc697345ec8fa434ae65dd32d7a3019485eb
clamp <15528374+DJ-clamp@users.noreply.github.com>
2022-11-26 15:14:36 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-import axios from "axios";
 import { aldiBeers } from "./aldi.js";
 import { tescoBeers } from "./tesco.js";
 import fs from 'fs'
 let result = []
 
-let price1 = await aldiBeers()
-result.push(price1)
+// let price1 = await aldiBeers()
+// result.push(price1)
 let price2 = await tescoBeers()
 result.push(price2)
 console.log(result)
